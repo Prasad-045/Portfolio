67e210e92aac57988158cf4113805d7a1e7e8a6d
Prasad-045 <gorigeprasad8@gmail.com>
2024-11-14 18:33:53 +0530

    first commit

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function fn(){
     document.getElementById("arrow").style.backgroundColor="black"
 
 }
+function toggleMenu(){
+    var menu = document.querySelector(".menu-links");
+    var icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
